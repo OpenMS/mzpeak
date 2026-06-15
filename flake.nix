8f6036be00ab840321666b9aea023e3f7ce8aa60
Peter J. Jones <peter@jonesbunch.com>
2026-06-15 16:32:52 +0200

    nix: Add a way to build arrow/parquet with debugging symbols

--- flake.nix
+++ flake.nix
@@ -30,9 +30,18 @@
             stdenv = if pkgs.stdenv.isDarwin then pkgs.overrideCC pkgs.stdenv pkgs.clang_22 else pkgs.stdenv;
           };
 
+          # Always use clang.
           mzpeak_clang = self.packages.${system}.mzpeak.override (_: {
-            # Always use clang.
             stdenv = pkgs.overrideCC pkgs.stdenv pkgs.clang_22;
+          });
+
+          # Build dependencies with debugging symbols.
+          mzpeak_debug = self.packages.${system}.mzpeak.override (_: {
+            arrow-cpp = pkgs.arrow-cpp.overrideAttrs (orig: {
+              cmakeBuildType = "RelWithDebInfo";
+              dontStrip = true;
+              hardeningDisable = (orig.hardeningDisable or [ ]) ++ [ "fortify" ];
+            });
           });
         }
       );
@@ -40,17 +49,24 @@
       checks = each (pkgs: system: self.packages.${system});
 
       devShells = each (
-        pkgs: system: {
-          default = pkgs.mkShell {
-            name = "mzpeak shell";
-            hardeningDisable = [ "fortify" ];
-            inputsFrom = [ self.packages.${system}.default ];
-            buildInputs = [
-              pkgs.clang-tools
-              pkgs.python3
-              pkgs.ruff
-            ];
-          };
+        pkgs: system:
+        let
+          shell =
+            mzpeak:
+            pkgs.mkShell {
+              name = "mzpeak shell";
+              hardeningDisable = [ "fortify" ];
+              inputsFrom = [ mzpeak ];
+              buildInputs = [
+                pkgs.clang-tools
+                pkgs.python3
+                pkgs.ruff
+              ];
+            };
+        in
+        {
+          default = shell self.packages.${system}.default;
+          debug = shell self.packages.${system}.mzpeak_debug;
         }
       );
     };
