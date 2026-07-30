83a740c248d711828a46a511434c9e6ea462eb76
Peter J. Jones <peter@jonesbunch.com>
2026-07-30 15:25:27 +0200

    signals: Rename (field -> column) and overload function
    The name `column` is more appropriate, and the overloads reduce code
    bloat in other compilation units.

--- src/data/signals.cpp
+++ src/data/signals.cpp
@@ -102,9 +102,23 @@ std::size_t Signals::record_count() const
 }
 
 /******************************************************************************/
-std::optional<Schema::Column> Signals::field(const std::string_view& name) const
+std::optional<Schema::Column> Signals::column(const std::string_view& name) const
 {
   return impl_->parquet_->field(impl_->array_index_->prefix(), name);
+}
+
+/******************************************************************************/
+std::optional<Schema::Column> Signals::column(const ArrayIndex::Entry& entry) const
+{
+  return impl_->array_index_->entry_column(*impl_->parquet_->groups(), entry);
+}
+
+/******************************************************************************/
+std::optional<Schema::Column> Signals::column(const ArrayIndex::Dimension& dim,
+                                              Schema::BufferFormat format) const
+{
+  return dim.entry_with(format).and_then(
+      [this](const auto& entry) { return column(entry); });
 }
 
 /******************************************************************************/
@@ -118,7 +132,7 @@ Util::Query::Builder Signals::index() const
 {
   auto entity_type = impl_->array_index_->entity_type();
   auto field_name = Schema::entity_type_to_string(entity_type) + "_index";
-  auto index_field = field(field_name);
+  auto index_field = column(field_name);
 
   if (!index_field.has_value()) {
     throw ParquetError("parquet file is missing the index column: " + field_name);
