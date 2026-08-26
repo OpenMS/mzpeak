e2c732ae95f3520691b6ca226b53fd0c9d0e6084
Peter J. Jones <peter@jonesbunch.com>
2026-08-26 14:50:33 +0200

    data_kind: Convert to a proper class with an enum subtype
    This allows us to record the original string value of the data kind if
    we don't know how to parse it.

--- include/mzpeak/schema/file.h
+++ include/mzpeak/schema/file.h
@@ -57,7 +57,7 @@ public:
 
 private:
   std::string file_name_;
-  DataKind data_kind_ = DataKind::Other;
+  DataKind data_kind_ = DataKind("other");
   EntityType entity_type_ = EntityType::Other;
   std::vector<Column> columns_;
 };
